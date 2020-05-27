d34d6cb6ac12d7cc9f1f3a15733c2c5e4b419dea
singllord <864376396@qq.com>
2020-05-27 21:15:03 +0800

    change test

--- src/main/java/com/example/demo1/ma.java
+++ src/main/java/com/example/demo1/ma.java
@@ -14,6 +14,6 @@ import org.springframework.web.servlet.ModelAndView;
 public class ma {
     @RequestMapping(value = "/")
     public String main(){
-        return "hollo";
+        return "hollo changed";
     }
 }
